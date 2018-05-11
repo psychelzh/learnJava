4735980fa8ad2a5dd0ae82f1c0009091eb69f2a8
Zhang Liang <psychelzh@outlook.com>
2018-05-11 21:19:04 +0800

    Add comments for test

--- src/test/java/learn/algs/problemTest.java
+++ src/test/java/learn/algs/problemTest.java
@@ -1,7 +1,15 @@
 package learn.algs;
 import edu.princeton.cs.algs4.*;
 import org.junit.Test;
+/**
+ * This class is used to test all the code exercises for algs4.
+ *
+ * Each test is named as <code>test####</code>, in which <code>####</code>
+ * represents the exercise order.
+ */
 public class problemTest {
+
+    // test for 1.1.7a
     @Test
     public void test117a() {
         double t = 9.0;
