8a50df60f17b10682bf742465baa5b7958348363
Zhang Liang <psychelzh@outlook.com>
2018-05-11 16:23:14 +0800

    Inital commit

---
+++ src/main/java/learn/algs/problemTest.java
@@ -0,0 +1,10 @@
+package learn.algs;
+import edu.princeton.cs.algs4.*;
+public class problemTest {
+    public static void main(String[] args) {
+        double t = 9.0;
+        while (Math.abs(t - 9.0/t) > .001)
+            t = (9.0/t + t) / 2.0;
+        StdOut.printf("%.5f\n", t);
+    }
+}
