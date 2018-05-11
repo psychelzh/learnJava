8465b5ea481fad639a20082805b02070af47c98b
Zhang Liang <psychelzh@outlook.com>
2018-05-11 19:16:53 +0800

    Change problemTest to test module

--- src/test/java/learn/algs/problemTest.java
+++ src/test/java/learn/algs/problemTest.java
@@ -1,7 +1,9 @@
 package learn.algs;
 import edu.princeton.cs.algs4.*;
+import org.junit.Test;
 public class problemTest {
-    public static void main(String[] args) {
+    @Test
+    public void test117a() {
         double t = 9.0;
         while (Math.abs(t - 9.0/t) > .001)
             t = (9.0/t + t) / 2.0;
