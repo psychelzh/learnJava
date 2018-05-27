40c8015ceeb9d92d92932fe325b2344d6bfd6459
Liang Zhang <psychelzh@outlook.com>
2018-05-27 21:36:45 +0800

    Completed the first version of implementation that passed tests

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -8,14 +8,16 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-  WeightedQuickUnionUF sites; // represents all the sites
-  int dim;                    // number of rows/columns
-  boolean[] sitesIsOpen;      // represents open status for all sites
+  private WeightedQuickUnionUF sites; // represents all the sites
+  private int dim; // number of rows/columns
+  private boolean[] sitesIsOpen; // represents open status for all sites
+  private int topVSite; // index of top virtual site
+  private int bottomVSite; // index of bottom virtual site
 
   /**
    * Constructor method.
    *
-   * <p>create n-by-n grid, with all sites blocked
+   * <p>create n-by-n grid, with all sites blocked.
    *
    * @param n The number of rows/columns.
    * @throws IllegalArgumentException If the number of rows/columns input is not larger than 0, this
@@ -30,13 +32,10 @@ public class Percolation {
     // initialize as closed for all sites
     sitesIsOpen = new boolean[dim * dim];
     // there are two virtual sites
-    sites = new WeightedQuickUnionUF(n * n + 2);
-    for (int i = 1; i <= n; i++) {
-      // the first element denotes the top virtual site, connect it to the top row
-      sites.union(0, i);
-      // the last element denotes the bottom virtual site, connect it to the bottom row
-      sites.union(n * n + 1, n * n - i + 1);
-    }
+    sites = new WeightedQuickUnionUF(dim * dim + 2);
+    // the last two sites is the virtual sites
+    topVSite = dim * dim;
+    bottomVSite = dim * dim + 1;
   }
 
   /**
@@ -45,25 +44,36 @@ public class Percolation {
   public void open(int row, int col) {
     validate(row);
     validate(col);
-    int thisSiteIndex = (row - 1) * dim + col;
     // mark the status of this site as open
+    int thisSiteIndex = sub2ind(row, col);
     sitesIsOpen[thisSiteIndex] = true;
     // connect this site to all the adjacent open sites
-    if (row > 1) {
+    if (row > 1 && isOpen(row - 1, col)) {
       // not a site from the first row, connect to the upper site
-      sites.union(thisSiteIndex, thisSiteIndex - dim);
+      sites.union(thisSiteIndex, sub2ind(row - 1, col));
     }
-    if (row < dim) {
-      // not a site from the last row, connect to the below site
-      sites.union(thisSiteIndex, thisSiteIndex + dim);
+    if (row < dim && isOpen(row + 1, col)) {
+      // not a site from the last row, connect to the lower site
+      sites.union(thisSiteIndex, sub2ind(row + 1, col));
     }
-    if (col > 1) {
+    if (col > 1 && isOpen(row, col - 1)) {
       // not a site from the first column, connect to the left site
-      sites.union(thisSiteIndex, thisSiteIndex - 1);
+      sites.union(thisSiteIndex, sub2ind(row, col - 1));
     }
-    if (col < dim) {
+    if (col < dim && isOpen(row, col + 1)) {
       // not a site from the last column, connect to the right site
-      sites.union(thisSiteIndex, thisSiteIndex + 1);
+      sites.union(thisSiteIndex, sub2ind(row, col + 1));
+    }
+    // connect site from the first row to top virtual site when open
+    if (row == 1) {
+      sites.union(thisSiteIndex, topVSite);
+    }
+    // connect any full site in the last row to the bottom virtual site
+    for (int i = 1; i <= dim; i++) {
+      if (isFull(dim, i)) {
+        // connect site from the last row to bottom virtual site when open
+        sites.union(sub2ind(dim, i), bottomVSite);
+      }
     }
   }
 
@@ -73,28 +83,37 @@ public class Percolation {
   public boolean isOpen(int row, int col) {
     validate(row);
     validate(col);
-    return sitesIsOpen[(row - 1) * dim + col];
+    return sitesIsOpen[sub2ind(row, col)];
   }
 
   /**
    * Check whether site ({@code row}, {@code col}) is <b>full</b> or not.
    */
   public boolean isFull(int row, int col) {
-    return false;
+    validate(row);
+    validate(col);
+    return sites.connected(topVSite, sub2ind(row, col));
   }
 
   /**
    * Get the number of open sites.
    */
   public int numberOfOpenSites() {
-    return 0;
+    int count = 0;
+    for (boolean siteIsOpen : sitesIsOpen) {
+      if (siteIsOpen) {
+        count++;
+      }
+    }
+    return count;
   }
 
   /**
    * Check whether the system percolates or not.
    */
   public boolean percolates() {
-    return false;
+    // the system percolates when the top and bottom virtual sites are connected
+    return sites.connected(topVSite, bottomVSite);
   }
 
   // validate that p is a valid row/column index
@@ -102,6 +121,13 @@ public class Percolation {
     if (p <= 0 || p > dim) {
       throw new IllegalArgumentException("index " + p + " is not between 1 and " + dim);
     }
+  }
+
+  // convert row/column index to site order index
+  private int sub2ind(int row, int col) {
+    validate(row);
+    validate(col);
+    return (row - 1) * dim + col - 1;
   }
 
   /**
