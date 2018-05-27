f9cdba87e59b54e8b7e62e56752fbb7889a74733
Liang Zhang <psychelzh@outlook.com>
2018-05-27 21:54:46 +0800

    Remove two unecessary imports

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -2,8 +2,6 @@
  * Percolation Liang, Zhang
  */
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
