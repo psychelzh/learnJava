0dfd1b44cce2e39d68e137ed7b9d414f620a0aa8
Liang Zhang <psychelzh@outlook.com>
2018-05-31 09:16:34 +0800

    Add package info for learn.algs package

---
+++ src/main/java/learn/algs/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The package is used to do the exercises of the Algorithm book by Robert
+ * Sedgewick and Kevin Wayne.
+ *
+ * @author Liang Zhang
+ * @version null
+ */
+
+ package learn.algs;
