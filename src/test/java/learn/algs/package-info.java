3de3f8f738c5b89b9e14ebdf6ff99194209e9ab8
Liang Zhang <psychelzh@outlook.com>
2018-06-18 16:18:31 +0800

    Remove duplicate package info

--- src/test/java/learn/algs/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The package is used to do the exercises of the Algorithm book by Robert
- * Sedgewick and Kevin Wayne.
- *
- * @author Liang Zhang
- * @version null
- */
-
- package learn.algs;
